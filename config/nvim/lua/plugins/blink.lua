5474f6e7a8f1ce4615f7c3abf96d48368ba5f74c
Aimad Ahsan <ahsanaimad@gmail.com>
2025-01-04 20:58:04 +0530

    Remove dressing.lua and snacks keybindings (for now)

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -35,6 +35,7 @@ return {
         -- Default list of enabled providers defined so that you can extend it
         sources = {
             default = { 'lsp', 'path', 'snippets', 'buffer' },
+            cmdline = {},
         },
     },
     opts_extend = { "sources.default" }
